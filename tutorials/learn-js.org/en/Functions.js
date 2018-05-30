2ed2fb4bc3e9574ea0d28d2f8eae051a8296f3a7
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:00:35 -0500

    Changed Functions.md to Functions.js
    Changed Functions.md to Functions.js & implemented Solution

---
+++ tutorials/learn-js.org/en/Functions.js
@@ -0,0 +1,15 @@
+// Define a function called `multiplyFive` which accepts a number and returns that number multiplied by 5.
+
+/* EXAMPLE */
+console.log(multiplyFive(3));
+console.log(multiplyFive(4));
+console.log(multiplyFive(5));
+console.log(multiplyFive(6));
+
+/* SOLUTION */
+let multiplyFive = num => num * 5;
+
+console.log(multiplyFive(3)); //15
+console.log(multiplyFive(4)); //20
+console.log(multiplyFive(5)); //25
+console.log(multiplyFive(6)); //30
