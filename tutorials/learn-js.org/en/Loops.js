d046e1df73258063efadacfab9e930aca0800245
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:15:44 -0500

    Changed Loops.md to Loops.js
    Changed Loops.md to Loops.js & implemented solution

---
+++ tutorials/learn-js.org/en/Loops.js
@@ -0,0 +1,20 @@
+// In this exercise, you must write a for loop that iterates on the myArray variable and prints out all of its members.
+
+/* EXAMPLE 
+var myArray = ["What is the meaning of life?", "The meaning of life is", 42];
+
+// TODO: modify this code
+for ()
+{
+}
+*/
+
+
+/* SOLUTION */
+let myArray = [
+    "What is the meaning of life?",
+    "The meaning of life is", 
+    42
+];
+
+myArray.forEach((value)=>console.log(value));
