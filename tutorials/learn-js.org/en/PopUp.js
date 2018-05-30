38132516999e33e76ceb4becf1630db66607835d
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:41:16 -0500

    Changed Pop-up Boxes.md to PopUp.js
    Changed Pop-up Boxes.md to PopUp.js & implemented solution

---
+++ tutorials/learn-js.org/en/PopUp.js
@@ -0,0 +1,8 @@
+/*
+    Make a variable ```test``` set it equal to a prompt box, and type "Hi!" in it (without the quotes) when it pops up. 
+    Note: your pop-up blocker must not be enabled.
+*/
+
+/* SOLUTION */
+let test = prompt();
+console.log(test);
