c8173aa347c9f6b712fc80570cba07976113da01
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:25:15 -0500

    Changed Object Oriented Javascript.md to ObjectOriented.js
    Changed Object Oriented Javascript.md to ObjectOriented.js & implemented Solution

---
+++ tutorials/learn-js.org/en/ObjectOriented.js
@@ -0,0 +1,28 @@
+/*
+    Create a class called Person which accepts the name of a person as a string, and his/her age as a number. 
+
+The Person class should have a method called `describe` which returns a string with the following syntax: 
+    "`name`, `age` years old". 
+So for example, if John is 19 years old then the function `describe` of his object will return "John, 19 years old".
+
+EXAMPLE
+// TODO: create the Person class using a function
+
+var jack = new Person("Jack", 25);
+var jill = new Person("Jill", 24);
+console.log(jack.describe());
+console.log(jill.describe());
+*/
+
+
+
+/* SOLUTION */
+var Person = function(name, age){
+    this.name = name;
+    this.age = age;
+    this.describe = () => `${this.name}, ${this.age} years old`;
+}
+var jack = new Person("Jack", 25);
+var jill = new Person("Jill", 24);
+console.log(jack.describe());
+console.log(jill.describe());
