e34ec26eccb7d540fec0dad56b907767f114d0c9
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:00:52 -0500

    Update Functions.js

--- tutorials/learn-js.org/en/Functions.js
+++ tutorials/learn-js.org/en/Functions.js
@@ -1,10 +1,11 @@
 // Define a function called `multiplyFive` which accepts a number and returns that number multiplied by 5.
 
-/* EXAMPLE */
+/* EXAMPLE
 console.log(multiplyFive(3));
 console.log(multiplyFive(4));
 console.log(multiplyFive(5));
 console.log(multiplyFive(6));
+*/
 
 /* SOLUTION */
 let multiplyFive = num => num * 5;
