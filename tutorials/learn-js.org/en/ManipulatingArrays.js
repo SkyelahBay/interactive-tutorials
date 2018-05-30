d03f0805df53e7f0ca5d8115d66cf69ab4101339
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:18:33 -0500

    Changed Manipulating Arrays.md to ManipulatingArrays.js
    Changed Manipulating Arrays.md to ManipulatingArrays.js & implemented solution

---
+++ tutorials/learn-js.org/en/ManipulatingArrays.js
@@ -0,0 +1,23 @@
+/* In this tutorial, you must manipulate the array called `myArray` and do the following:
+    1. Push the number 42 to the end of the array   2. Shift a variable from the beginning of the array
+
+EXAMPLE
+var myArray = [true, "What is the meaning of life?"];
+// TODO: write your code here:
+
+console.log(myArray[0]);
+console.log(myArray[1]);
+*/
+
+
+/* SOLUTION */
+let myArray = [
+    true, 
+    "What is the meaning of life?"
+];
+
+myArray.shift();
+myArray.push(42);
+
+console.log(myArray[0]); //"What is the meaning of life?"
+console.log(myArray[1]); // 42
