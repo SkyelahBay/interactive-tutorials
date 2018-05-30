8c3b122ab2918d1eaae379323e2c91b5fdb981be
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 11:27:01 -0500

    Update ObjectOriented.js

--- tutorials/learn-js.org/en/ObjectOriented.js
+++ tutorials/learn-js.org/en/ObjectOriented.js
@@ -17,12 +17,11 @@ console.log(jill.describe());
 
 
 /* SOLUTION */
-var Person = function(name, age){
+let Person = function(name, age){
     this.name = name;
     this.age = age;
     this.describe = () => `${this.name}, ${this.age} years old`;
 }
-var jack = new Person("Jack", 25);
-var jill = new Person("Jill", 24);
-console.log(jack.describe());
-console.log(jill.describe());
+let jack = new Person("Jack", 25);
+let jill = new Person("Jill", 24);
+console.log(`${jack.describe()}\n${jill.describe()}`);
