dcabe4dd2802cd9ec30725a8e7bb05f3ab2d9316
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 10:55:55 -0500

    Changed Function Context.md to FunctionContext.js
    Changed 'Function Context.md' to FunctionContext.js & implemented solution

---
+++ tutorials/learn-js.org/en/FunctionContext.js
@@ -0,0 +1,52 @@
+/*
+    Create bound copies of printFullName and printDetails to person called boundPrintFullName and boundPrintDetails.
+*/
+
+/* EXAMPLE */
+var person = {
+    firstName : "John",
+    lastName : "Smith",
+    age : 23
+};
+function printFullName()
+{
+    console.log(this.firstName + " " + this.lastName);
+}
+
+function printDetails()
+{
+    console.log(this.firstName + " is " + this.age + " years old");
+}
+
+// TODO: create bound copies of printFullName and printDetails.
+var boundPrintFullName;
+var boundPrintDetails;
+boundPrintFullName();
+boundPrintDetails();
+
+
+
+
+
+/* SOLUTION */
+
+let person = {
+    firstName : "Skyelah",
+    lastName : "Bay",
+    age : 24
+};
+
+function printFullName()
+{
+    console.log(`${this.firstName} ${this.lastName}`);
+}
+function printDetails()
+{
+    console.log(`${this.firstName} is ${this.age} years old"`);
+}
+
+let boundPrintFullName = printFullName.bind(person);
+let boundPrintDetails = printDetails.bind(person);
+
+boundPrintFullName(); //Skyelah Bay
+boundPrintDetails();  //Skyelah is 24 years old
