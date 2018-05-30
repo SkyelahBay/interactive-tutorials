6eda7181dca0a7178a3475b1c32edc03fb5d985b
Skyelah Bay <skyelahbay@gmail.com>
2018-05-30 10:37:07 -0500

    changed Arrays.md to Arrays.js
    changed Arrays.md to Arrays.js & implemented solution

---
+++ tutorials/learn-js.org/en/Arrays.js
@@ -0,0 +1,14 @@
+
+/* TODO: change myArray to contain the variables
+var myArray = [];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+*/
+
+
+/ * SOLUTION * /
+let myArray = ["What is the meaning of life?", 42, true];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
